2ded51341a7dd6507554aca7df645fa5f9f4a16c
imamachi-alda <naotoimamachi@aldagram.com>
2026-02-10 22:16:00 +0900

    feat: GitHub Actions経由でGitHub Pages のデプロイをできるようにした

--- packages/@ai-suburi/docs/docusaurus.config.ts
+++ packages/@ai-suburi/docs/docusaurus.config.ts
@@ -15,15 +15,14 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://Imamachi-n.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/genai-agent-advanced-book-typescript/',
 
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Imamachi-n',
+  projectName: 'genai-agent-advanced-book-typescript',
 
   onBrokenLinks: 'throw',
 
